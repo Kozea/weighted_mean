b532ca2b59783f71aeb61d7acb6dacfc46dee78b
Ronan Dunklau <ronan.dunklau@kozea.fr>
2012-04-03 11:54:43 +0200

    Formatting

--- src/weighted_mean.c
+++ src/weighted_mean.c
@@ -15,9 +15,9 @@ PG_FUNCTION_INFO_V1(_weighted_mean_final);
 
 typedef struct WeightedMeanInternalState
 {
-	Numeric running_sum;
-	Numeric running_amount;
-    MemoryContext mcontext;
+	Numeric		running_sum;
+	Numeric		running_amount;
+	MemoryContext mcontext;
 }	WeightedMeanInternalState;
 
 
@@ -25,13 +25,14 @@ Datum
 _weighted_mean_final(PG_FUNCTION_ARGS)
 {
 	WeightedMeanInternalState *state;
-    MemoryContext oldcontext;
-	Datum total;
+	MemoryContext oldcontext;
+	Datum		total;
+
 	state = (WeightedMeanInternalState *) PG_GETARG_POINTER(0);
-    oldcontext = MemoryContextSwitchTo(state->mcontext);
+	oldcontext = MemoryContextSwitchTo(state->mcontext);
 	total = DirectFunctionCall2(numeric_div, NumericGetDatum(state->running_sum), NumericGetDatum(state->running_amount));
-    pfree(state);
-    MemoryContextSwitchTo(oldcontext);
+	pfree(state);
+	MemoryContextSwitchTo(oldcontext);
 	PG_RETURN_NUMERIC(total);
 }
 
@@ -39,42 +40,42 @@ Datum
 _weighted_mean_intermediate(PG_FUNCTION_ARGS)
 {
 	WeightedMeanInternalState *state;
-	Datum   value,
-			amount,
-            temp_total;
-    MemoryContext aggcontext,
-                  oldcontext,
-                  internalcontext;
+	Datum		value,
+				amount,
+				temp_total;
+	MemoryContext aggcontext,
+				oldcontext,
+				internalcontext;
 
 
-    if (!AggCheckCallContext(fcinfo, &aggcontext))
+	if (!AggCheckCallContext(fcinfo, &aggcontext))
 	{
 		/* cannot be called directly because of internal-type argument */
-		elog(ERROR, "array_agg_transfn called in non-aggregate context");
+		elog(ERROR, "_weighted_mean_intermediate called in non-aggregate context");
 	}
 	if (PG_ARGISNULL(0))
 	{
-        internalcontext = AllocSetContextCreate(aggcontext, "WeightedMeanState", 
-              ALLOCSET_DEFAULT_MINSIZE,
-              ALLOCSET_DEFAULT_INITSIZE,
-              ALLOCSET_DEFAULT_MAXSIZE);
-        oldcontext = MemoryContextSwitchTo(internalcontext);
+		internalcontext = AllocSetContextCreate(aggcontext, "WeightedMeanState",
+												ALLOCSET_DEFAULT_MINSIZE,
+												ALLOCSET_DEFAULT_INITSIZE,
+												ALLOCSET_DEFAULT_MAXSIZE);
+		oldcontext = MemoryContextSwitchTo(internalcontext);
 		state = (WeightedMeanInternalState *) palloc(sizeof(WeightedMeanInternalState));
-        state->mcontext = internalcontext;
+		state->mcontext = internalcontext;
 		state->running_sum = DatumGetNumeric(DirectFunctionCall1(numeric_in, CStringGetDatum("0")));
 		state->running_amount = DatumGetNumeric(DirectFunctionCall1(numeric_in, CStringGetDatum("0")));
 	}
 	else
 	{
 		state = (WeightedMeanInternalState *) PG_GETARG_POINTER(0);
-        oldcontext = MemoryContextSwitchTo(state->mcontext);
+		oldcontext = MemoryContextSwitchTo(state->mcontext);
 	}
 	value = PG_GETARG_DATUM(1);
 	amount = PG_GETARG_DATUM(2);
-    temp_total = DirectFunctionCall2(numeric_mul, value, amount);
+	temp_total = DirectFunctionCall2(numeric_mul, value, amount);
 	state->running_sum = DatumGetNumeric(DirectFunctionCall2(numeric_add, NumericGetDatum(state->running_sum), temp_total));
 	state->running_amount = DatumGetNumeric(DirectFunctionCall2(numeric_add,
 							NumericGetDatum(state->running_amount), amount));
-    MemoryContextSwitchTo(oldcontext);
+	MemoryContextSwitchTo(oldcontext);
 	PG_RETURN_POINTER(state);
 }
